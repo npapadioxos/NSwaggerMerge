25bdd475c9e84316fb8f881d90ab45b08bbc74fb
Nick Papadiochos <npapadiochos@singularlogic.eu>
2025-08-13 09:35:49 +0300

    chore: 🚧 Drop load from and save to swagger doc physical files option

--- src/Serialization/JsonFile.cs
+++ src/Serialization/JsonFile.cs
@@ -11,14 +11,6 @@ internal static class JsonFile
         MetadataPropertyHandling = MetadataPropertyHandling.Ignore
     };
 
-    public static async Task<T> LoadFileAsync<T>(string filePath) where T : class
-    {
-        var content = await File.ReadAllTextAsync(filePath);
-        var deserializedContent = JsonConvert.DeserializeObject<T>(content, Settings);
-
-        return deserializedContent ?? throw new InvalidOperationException($"File '{filePath}' could not be loaded correctly as it is not in the correct format.");
-    }
-
     public static async Task<T> LoadRemoteFileAsync<T>(string httpPath) where T : class
     {
         using HttpClient client = new();
@@ -26,12 +18,6 @@ internal static class JsonFile
         var deserializedContent = JsonConvert.DeserializeObject<T>(response, Settings);
 
         return deserializedContent ?? throw new InvalidOperationException($"HttpFile '{httpPath}' could not be loaded correctly as it is not in the correct format.");
-    }
-
-    public static async Task SaveFileAsync<T>(string filePath, T data) where T : class
-    {
-        var content = JsonConvert.SerializeObject(data, Settings);
-        await File.WriteAllTextAsync(filePath, content);
     }
 
     public static string OutJsonAsync<T>(T data) where T : class =>
